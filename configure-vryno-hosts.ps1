964d4c7d657db5209d545311b542f089da6ffc78
Mohit Thakral <mohit@targetintegration.com>
2021-11-29 10:29:51 -0800

    Add vryno hosts

--- configure-vryno-hosts.ps1
+++ configure-vryno-hosts.ps1
@@ -1,5 +1,7 @@
 $WSL_IP=wsl hostname -I
 
+.\RemoveFromHosts.ps1 -Hostname llog.vryno.dev
+
 .\RemoveFromHosts.ps1 -Hostname lapp.vryno.dev
 .\RemoveFromHosts.ps1 -Hostname lms.vryno.dev
 
@@ -8,6 +10,9 @@ $WSL_IP=wsl hostname -I
 
 .\RemoveFromHosts.ps1 -Hostname oreo1.lapp.vryno.dev
 .\RemoveFromHosts.ps1 -Hostname oreo1.lms.vryno.dev
+
+
+.\AddToHosts.ps1 -Hostname llog.vryno.dev -DesiredIP $WSL_IP
 
 .\AddToHosts.ps1 -Hostname lapp.vryno.dev -DesiredIP $WSL_IP
 .\AddToHosts.ps1 -Hostname lms.vryno.dev -DesiredIP $WSL_IP
